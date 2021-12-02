2ce85e82e847c207a833faa7ad5766787a488fb7
Jacob Kristensson <37665180+JackeJacob@users.noreply.github.com>
2021-12-02 15:35:40 +0100

    Update search.java

--- search.java
+++ search.java
@@ -7,7 +7,7 @@ public class search {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(args[1]));
         while(scan.hasNext()){
-            String line = scan.nextLine().toLowerCase().toString();
+            String line = scan.nextLine().toLowerCase();
             if(line.contains(args[0])){
                 System.out.println(line);
             }
